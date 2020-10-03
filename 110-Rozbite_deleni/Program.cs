38654b71b6b27217ed39618f7cb3f4a72e5bd50b
Jan Koupil <jan.koupil@delta-skola.cz>
2020-10-03 22:28:12 +0200

    Poslední 2 úlohy

---
+++ 110-Rozbite_deleni/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _110_Rozbite_deleni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Problém je v použití datového typu int - celých čísel.
+            // Při celočíselném dělení bude posíl 32 / 3 vypočten jako 10
+            // Opravíme použitím datového typu double
+
+            //int citatel = 32;
+            //int jmenovatel = 3;
+            double citatel = 32;
+            double jmenovatel = 3;
+
+            // Zde začíná záhada
+
+            //int podil = citatel / jmenovatel;
+            //int vysledekZpatky = podil * jmenovatel;
+            double podil = citatel / jmenovatel;
+            double vysledekZpatky = podil * jmenovatel;
+
+            // Zde záhada končí
+
+            Console.WriteLine($"{citatel} / {jmenovatel} * {jmenovatel} = {vysledekZpatky}");
+
+
+        }
+    }
+}
---
+++ 120-Heronuv_vzorec/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _120_Heronuv_vzorec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej stranu A: ");
+            double stranaA = double.Parse(Console.ReadLine());
+
+            Console.Write("Zadej stranu B: ");
+            double stranaB = double.Parse(Console.ReadLine());
+
+            Console.Write("Zadej stranu C: ");
+            double stranaC = double.Parse(Console.ReadLine());
+
+            double s = (stranaA + stranaB + stranaC) / 2;
+            double obsah = Math.Sqrt(s * (s- stranaA) * (s - stranaB) * (s-stranaC) ) ;
+
+            Console.WriteLine($"Obsah trojúhelníka o stranách {stranaA} , {stranaB} a {stranaC} je {obsah}.");
+        }
+    }
+}
