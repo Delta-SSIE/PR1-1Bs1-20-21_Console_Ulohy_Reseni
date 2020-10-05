646718084eaea65348f11833cd2abf597857fc91
Jan Koupil <jan.koupil@delta-skola.cz>
2020-10-05 11:26:55 +0200

    Základ řešení některých podmínek - příprava na case

---
+++ 02-Podm-060-Den_v_tydnu/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _02_Podm_060_Den_v_tydnu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej číslo dne (1-7): ");
+            int cislo = int.Parse(Console.ReadLine());
+
+            string den;
+
+            if (cislo == 1)
+            {
+                den = "pondělí";
+            } 
+            else if (cislo == 2)
+            {
+                den = "úterý";
+            }
+            else if (cislo == 3)
+            {
+                den = "středa";
+            }
+            else if (cislo == 4)
+            {
+                den = "čtvrtek";
+            }
+            else if (cislo == 5)
+            {
+                den = "pátek";
+            }
+            else if (cislo == 6)
+            {
+                den = "sobota";
+            }
+            else
+            {
+                den = "neděle";
+            }
+
+            Console.WriteLine($"Dnes je {den}.");
+        }
+    }
+}
---
+++ 02-Podm-090-Je_vikend/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02_Podm_090_Je_vikend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej číslo dne (1-7): ");
+            int cislo = int.Parse(Console.ReadLine());
+
+            if (cislo > 0 && cislo < 6)
+            {
+                Console.WriteLine("Není víkend.");
+            }
+            else if (cislo > 5 && cislo < 8)
+            {
+                Console.WriteLine("Je víkend.");
+            }
+            else
+            {
+                Console.WriteLine("Toto není den v týdnu");
+            }
+        }
+    }
+}
