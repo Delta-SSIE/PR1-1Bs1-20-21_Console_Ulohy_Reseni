0211040b1f99ad66dd9316fc9b0d35719e3a9752
Jan Koupil <jan.koupil@delta-skola.cz>
2020-11-06 11:05:25 +0100

    Reseni While 140-170

---
+++ 03-While-140-Maximum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _03_While_140_Maximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadávej přirozená čísla, já zjistím, které bylo největší. Vložením 0 nebo záporného čísla sčítání ukončíš");
+
+            //připravím si průběžné maximum
+            int maximum = int.MinValue; // dám tam nejmenší myslitelnou hodnotu
+
+            // připravím proměnnou na načítání a poprvé do ní uložím
+            int nacteno = int.Parse(Console.ReadLine());
+
+            while (nacteno > 0) //dokud je to kladné, budu pracovat
+            {
+                if (nacteno > maximum) // dostal jsme číslo větší, než si pamatuju?
+                    maximum = nacteno; // uložím si ho jako nové maximum
+
+                nacteno = int.Parse(Console.ReadLine()); // načtu další číslo
+            }
+
+            if (maximum > int.MaxValue) // Změnila se hodnota od začátku programu?
+            {
+                // Pokud ano, je co vypisovat.
+                Console.WriteLine($"Nejmenší číslo z požadovaných bylo {maximum}.");
+            }
+            else
+            {
+                // Pokud ne, jen oznámím
+                Console.WriteLine($"Nebylo zadáno dost čísel, nemohl jsem si nic uložit.");
+            }
+
+        }
+    }
+}
---
+++ 03-While-170-Minimax/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _03_While_170_Minimax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sestavte program, který se uživatele ptá na čísla tak dlouho, dokud uživatel nevloží příkaz "X"
+            //Poté vypíše minimální a maximální a průměrné vložené číslo.
+            //Pozor, neplatné vstupy budou uživateli oznámeny jako chyba, program nespadne.
+
+            Console.WriteLine("Zadávej čísla, ukonči příkazem \"X\"");
+
+            double minimum = double.PositiveInfinity;
+            double maximum = double.NegativeInfinity;
+            double soucet = 0;
+            double pocet = 0;
+
+            Console.Write("Zadej první číslo: ");
+            string prikaz = Console.ReadLine();
+
+            while (prikaz.ToUpper() != "X")
+            {
+                if (double.TryParse(prikaz, out double cislo))
+                {
+                    soucet += cislo;
+                    pocet++;
+                    if (cislo < minimum)
+                        minimum = cislo;
+                    if (cislo > maximum) // Pozor, nelze použít else if. Uhodnete proč?
+                        maximum = cislo;
+
+                    Console.Write("Zadej další: ");
+                }
+                else
+                {
+                    Console.Write("To není číslo, zkus to znovu");
+                }
+
+                prikaz = Console.ReadLine();
+            }
+            if (pocet > 0)
+                Console.WriteLine($"Zadáno {pocet} čísel, nejmenší bylo {minimum}, největší {maximum}, průměr vložených čísel byl {soucet/pocet}.");
+            else
+                Console.WriteLine("Nebylo vloženo žádné číslo.");
+        }
+    }
+}
