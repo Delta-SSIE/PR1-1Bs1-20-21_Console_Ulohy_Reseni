7dc3cac52950bf73cd14fdc65ce82c6d0c167996
Jan Koupil <jan.koupil@delta-skola.cz>
2021-05-02 22:50:23 +0200

    Dictionary - base

---
+++ 13-Kolekce-050-Frekvence-znaku/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _13_Kolekce_050_Frekvence_znaku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //načtení textu
+            string text = System.IO.File.ReadAllText(@"temno.txt");
+            //výpis pro kontrolu, smažte si nebo zakomentujte
+            Console.WriteLine(text);
+
+        }
+    }
+}
